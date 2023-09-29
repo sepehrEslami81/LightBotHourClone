10b81bed483516c2463a0fc2b8d251071fb88f42
sepehr eslami <sepehr1381eslami@gmail.com>
2023-09-29 10:13:15 +0330

    change * -1 to -

--- Assets/Scripts/Model/Level/CubeTileModel.cs
+++ Assets/Scripts/Model/Level/CubeTileModel.cs
@@ -12,7 +12,7 @@ namespace Model.Level
         [SerializeField] private int height = 1;
 
         public int Height => height;
-        public Vector3 WorldPosition => new Vector3(position.x, 0, position.y * -1);
+        public Vector3 WorldPosition => new Vector3(position.x, 0, -position.y);
         public bool IsLightTile => isLightTile;
         public bool IsStartPoint => isStartPoint;
         public Position Position => position;
