86bcf8c328839885512155fbdbb8df9001ad34e3
sepehr eslami <sepehr1381eslami@gmail.com>
2023-09-29 18:45:59 +0330

    run / stop program

---
+++ Assets/Scripts/Presenter/Ui/ButtonsUiPresenter.cs
@@ -0,0 +1,40 @@
+﻿using Presenter.Procedure;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Presenter.Ui
+{
+    public class ButtonsUiPresenter : MonoBehaviour
+    {
+        [SerializeField] private ProcedurePresenter procedurePresenter;
+        
+        [Header("run/stop button references")]
+        [SerializeField] private Sprite runButtonSprite;
+        [SerializeField] private Sprite stopButtonSprite;
+        [SerializeField] private Image runStopButtonImage;
+
+        private bool _isRunning;
+        
+        public void RunOrStopButtonClickEvent()
+        {
+            _isRunning = !_isRunning;
+            
+            if(_isRunning)
+                Run();
+            else
+                Stop();
+        }
+
+        private void Run()
+        {
+            procedurePresenter.StartProgram();
+            runStopButtonImage.sprite = stopButtonSprite;
+        }
+        
+        private void Stop()
+        {
+            procedurePresenter.StopProgram();
+            runStopButtonImage.sprite = runButtonSprite;
+        }
+    }
+}
