34b11a19645d04be924db80577413fa02166930d
sepehr eslami <sepehr1381eslami@gmail.com>
2023-09-29 08:34:06 +0330

    remove useless threshold

--- Assets/Scripts/Presenter/Command/RotateRightCommandPresenter.cs
+++ Assets/Scripts/Presenter/Command/RotateRightCommandPresenter.cs
@@ -8,7 +8,6 @@ namespace Presenter.Command
     public class RotateRightCommandPresenter : MonoBehaviour, ICommand
     {
         [SerializeField] private RobotPresenter robotPresenter;
-        [Range(1, 360)] [SerializeField] private float rotationThreshold = 91f; // based on the tests
         
         public IEnumerator Execute()
         {
