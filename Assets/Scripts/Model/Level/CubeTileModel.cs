26c4092b40b41b8307d01e6f43d9e09437f55312
sepehr eslami <sepehr1381eslami@gmail.com>
2023-09-29 23:22:18 +0330

    update axis (remove -)

--- Assets/Scripts/Model/Level/CubeTileModel.cs
+++ Assets/Scripts/Model/Level/CubeTileModel.cs
@@ -13,7 +13,7 @@ namespace Model.Level
         [SerializeField] private int height = 1;
 
         public int Height => height;
-        public Vector3 WorldPosition => new Vector3(position.x, 0, -position.y);
+        public Vector3 WorldPosition => new Vector3(position.x, 0, position.y);
         public bool IsLightTile => isLightTile;
         public bool IsStartPoint => isStartPoint;
         public Position Position => position;
