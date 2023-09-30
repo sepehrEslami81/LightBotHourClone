e17c96457b5aefc97448a22a12c95d440d9b5faa
sepehr eslami <sepehr1381eslami@gmail.com>
2023-09-30 09:13:33 +0330

    add comment

--- Assets/Scripts/Model/Robot/RobotModel.cs
+++ Assets/Scripts/Model/Robot/RobotModel.cs
@@ -8,15 +8,18 @@ namespace Model.Robot
     {
         [SerializeField] private float robotHeight = .5f;
 
-        public float RobotHeight => robotHeight;
-        public Transform RobotGameObject => transform;
-        public Vector3 CurrentWorldPosition => transform.position;
         public Position Position { get; set; }
+        public float RobotHeight => robotHeight;
+        public Position StartPosition { get; set; }
+        public Transform RobotGameObject => transform;
+        public RobotDirection StartDirection { get; set; }
+        public Vector3 CurrentWorldPosition => transform.position;
         public RobotDirection Direction { get; set; } = RobotDirection.Forward;
+        
+        // In order to place the robot on the tile, the height of the robot is added to the Y axis.
+        // To get the current coordinates in the Y axis, we subtract the height of the robot from the current Y axis
         public int CurrentRobotYAxis => (int)(CurrentWorldPosition.y - RobotHeight);
 
-        public Position StartPosition { get; set; }
-        public RobotDirection StartDirection { get; set; }
 
         
     }
