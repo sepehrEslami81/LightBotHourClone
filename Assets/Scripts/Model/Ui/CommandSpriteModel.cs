1890db6dd7583fe65a516137cdae642d5c376648
sepehr eslami <sepehr1381eslami@gmail.com>
2023-09-30 09:14:04 +0330

    remove useless

--- Assets/Scripts/Model/Ui/CommandSpriteModel.cs
+++ Assets/Scripts/Model/Ui/CommandSpriteModel.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using Model.Commands;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Model.Ui
 {
     [Serializable]
     public class CommandSpriteModel
     {
-        [FormerlySerializedAs("commandName")] [SerializeField] private CommandName commandName;
+        [SerializeField] private CommandName commandName;
         [SerializeField] private Sprite iconSprite;
 
-        public CommandName CommandName => commandName;
         public Sprite IconSprite => iconSprite;
+        public CommandName CommandName => commandName;
     }
 }
